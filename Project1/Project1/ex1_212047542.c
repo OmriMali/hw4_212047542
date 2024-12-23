a70741ce3fdfd31da167ee66f3364459287cda8a
Omri Mali <mali1@mail.tau.ac.il>
2024-12-23 14:50:28 +0200

    finished

--- Project1/Project1/ex1_212047542.c
+++ Project1/Project1/ex1_212047542.c
@@ -16,41 +16,42 @@ typedef struct Student {
 
 // Function to create a new student
 Student* createStudent(int ID, const char* name, int age, float GPA) {
-	Student *stu;
-	if (!(stu = (Student*)malloc(sizeof(Student)))) {
-		stu->ID = ID;
-		strcpy(stu->name, *name);
-		stu->age = age;
-		stu->GPA = GPA;
-		stu->next = NULL;
+	Student *stu = NULL;
+	stu = (Student*)malloc(sizeof(Student));
+	if (stu == NULL) {
+		printf("Fatal error: memory allocation failed!\n");
+		exit;
 	}
+	stu->ID = ID;
+	strcpy(stu->name, name);
+	stu->age = age;
+	stu->GPA = GPA;
+	stu->next = NULL;
 }
 
 // Function to create a linked list of students
 Student* createStudentList() {
 	Student *list = NULL, *stu = NULL;
-	int ID, age,flag=0;
+	int i, num, ID, age,flag=0;
 	char name[50];
 	float GPA;
-	printf("Would you like to enter a new student to the list?\nYes: enter 1\nNo: enter 0\n");
-	scanf("%d", flag);
-	while (flag == 0) {
-		printf("Please enter the student's ID:\n");
-		scanf("%d", ID);
-		printf("Please enter the student's name:\n");
-		scanf("%s", name);
-		printf("Please enter the student's age:\n");
-		scanf("%d", age);
-		printf("Please enter the student's GPA:\n");
-		scanf("%f", GPA);
+	printf("Enter number of students: ");
+	scanf("%d", &num);
+	for (i=0;i<num;i++){
+		printf("Enter details for student %d:\n", i+1);
+		printf("ID: ");
+		scanf("%d", &ID);
+		printf("Name: ");
+		scanf("%50s", name);
+		printf("Age: ");
+		scanf("%d", &age);
+		printf("GPA: ");
+		scanf("%f", &GPA);
+		stu=createStudent(ID, &name, age, GPA);
 		stu->next = list;
 		list = stu;
-		printf("Would you like to enter a new student to the list?\nYes: enter 1\nNo: enter 0\n");
-		scanf("%d", flag);
-
 	}
-
-
+	return list;
 }
 
 
@@ -75,8 +76,9 @@ Student* findTopStudent(Student* head) {
 
 // Function to delete the student with the lowest GPA
 Student* deleteLowestGPA(Student* head) {
-	Student *min_GPA_stu, *curr_stu = head;
+	Student *curr_stu = NULL;
 	int min_GPA;
+	curr_stu = head;
 	if (curr_stu == NULL) {
 		printf("Students' list is empty!\n");
 		return NULL;
@@ -87,23 +89,43 @@ Student* deleteLowestGPA(Student* head) {
 			min_GPA = curr_stu->GPA;
 		curr_stu = curr_stu->next;
 	}
-	curr_stu = head;
-	while (curr_stu != NULL) {
-		if (curr_stu->next->GPA == min_GPA) {
-			min_GPA_stu = curr_stu->next;
-			curr_stu->next = curr_stu->next->next;
-			break;
-		}		
+	if (head->GPA == min_GPA) {
+		head = head->next;
+		return head;
 	}
-	return min_GPA_stu;
+	curr_stu = head;
+	while (curr_stu->next != NULL) {
+		if (curr_stu->next->GPA == min_GPA) {
+			curr_stu->next = curr_stu->next->next;
+			return head;
+		}	
+		curr_stu = curr_stu->next;
+	}
+	return head;
 }
-
+void printStudent(Student* stu) {
+	printf("ID: %d\n", stu->ID);
+	printf("Name: %s\n", stu->name);
+	printf("Age: %d\n", stu->age);
+	printf("GPA: %4.2f\n", stu->GPA);
+}
 // Function to print all students
 void printAllStudents(Student* head) {
+	Student *curr_stu = NULL;
+	curr_stu=head;
+	while (curr_stu != NULL) {
+		printStudent(curr_stu);
+		curr_stu = curr_stu->next;
+	}
 }
 
 // Function to free the memory of the entire student list
 void freeStudentList(Student* head) {
+	Student *curr_stu = head;
+	while (curr_stu != NULL) {
+		free(curr_stu);
+		curr_stu = curr_stu->next;
+	}
 }
 
 int main() {
